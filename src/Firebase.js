931db866db403017d6a0f9c8d7b546e4affde423
Ethan Massey <ejm4aa@virginia.edu>
2019-05-28 16:47:33 -0400

    Added Firebase.js

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASVxSTrcUoK2pma00tlKXgNX6Ml6bkmLA",
+  authDomain: "pomodoro-c0076.firebaseapp.com",
+  databaseURL: "https://pomodoro-c0076.firebaseio.com",
+  projectId: "pomodoro-c0076",
+  storageBucket: "pomodoro-c0076.appspot.com",
+  messagingSenderId: "1012255203862",
+  appId: "1:1012255203862:web:bd0ea1553ba3b4ee"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
