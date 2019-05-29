1f3715afeded70a4a8b3014b1dbd2e58aa83416e
Ethan Massey <ejm4aa@virginia.edu>
2019-05-29 13:26:47 -0400

    Added links from Home

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
+import firebase from "./Firebase";
 
 class Login extends Component {
   handleSignUp = async event => {
     event.preventDefault();
-    //const email = event.target.
+    const { email, password } = event.target.elements;
+    try {
+      const user = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email.value, password.value);
+      this.props.history.push("/");
+    } catch (error) {
+      alert(error);
+    }
   };
 
   render() {
