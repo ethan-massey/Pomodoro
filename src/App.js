0eeea64a9a319d12fcfcd69a8f0a958fce7d2c1a
Ethan Massey <ejm4aa@virginia.edu>
2019-05-29 11:30:36 -0400

    Changed Login.js to Home.js, which branches into Login.js and CreateAccount.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Route, BrowserRouter as Router} from 'react-router-dom';
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import Profile from "./Profile";
 import NewTask from "./NewTask";
 import LeaderBoards from "./LeaderBoards";
 import Help from "./Help";
+import Home from "./Home";
+import Login from "./Login";
+import CreateAccount from "./CreateAccount";
 //import Logout from ".Logout";
-import './App.css';
-
-
+import "./App.css";
 
 class App extends Component {
-
   render() {
-
     return (
-        <Router>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/CreateAccount" component={CreateAccount} />
         <Route exact path="/Profile" component={Profile} />
         <Route exact path="/NewTask" component={NewTask} />
 
@@ -24,15 +26,9 @@ class App extends Component {
 
         <Route exact path="/Help" component={Help} />
         {/* <Route exact path="/" component={Logout} /> */}
-        </Router>
-
+      </Router>
     );
-
   }
-
 }
 
-
-
 export default App;
-
