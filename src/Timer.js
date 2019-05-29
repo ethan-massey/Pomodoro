34008a353a5d2950e4ae77fddf8d8f575b319ca9
Kristen Maggard <ksm5k@virginia.edu>
2019-05-29 11:59:08 -0400

    working timergit add . yay

--- src/Timer.js
+++ src/Timer.js
@@ -6,7 +6,9 @@ class Timer extends React.Component {
     render() {
         return (
             <div>
-                Timer
+                <h1>
+                {this.props.minutes}:{this.props.seconds}
+                </h1>
             </div>
         )
     }
