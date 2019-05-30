319539b068b6e85b2e69fa3289e6c34e54f84223
Kristen Maggard <ksm5k@virginia.edu>
2019-05-30 14:59:56 -0400

    new design for home, login, and create account

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -3,7 +3,31 @@ import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import firebase from "./Firebase";
 
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  dense: {
+    marginTop: theme.spacing(2),
+  },
+  menu: {
+    width: 200,
+  },
+}));
+
 class CreateAccount extends Component {
+  classes = useStyles;
   handleSignUp = async event => {
     event.preventDefault();
     const { email, password } = event.target.elements;
@@ -19,25 +43,56 @@ class CreateAccount extends Component {
 
   render() {
     return (
-      <div>
+<div>
         <h1>Create your Pomodoro account</h1>
-        <form onSubmit={this.handleSignUp}>
-          <label>
-            Email
-            <input name="email" type="email" placeholder="Email" />
-          </label>
-          <label>
-            Password
-            <input name="password" type="password" placeholder="Password" />
-          </label>
-          <button type="submit">Sign up</button>
-        </form>
-        <br />
-        <button>
+        <form class="login" className={this.classes.container} noValidate autoComplete="off" onSubmit={this.handleSignUp}>
+      <TextField
+        id="outlined-name"
+        name="email"
+        label="Email"
+        type="email"
+        className={this.classes.textField}
+        margin="normal"
+        variant="outlined"
+      />
+            <TextField
+        id="outlined-name"
+        name="password"
+        label="Password"
+        type="password"
+        className={this.classes.textField}
+        margin="normal"
+        variant="outlined"
+      />
+      <Button type="submit">Sign up</Button>
+      </form>
+      <div class="login">
+        <Button>
           <Link to="/">Back</Link>
-        </button>
+        </Button>
       </div>
-    );
+      </div>
+
+      
+    //   <div>
+    //     <h1>Create your Pomodoro account</h1>
+    //     <form onSubmit={this.handleSignUp}>
+    //       <label>
+    //         Email
+    //         <input name="email" type="email" placeholder="Email" />
+    //       </label>
+    //       <label>
+    //         Password
+    //         <input name="password" type="password" placeholder="Password" />
+    //       </label>
+    //       <button type="submit">Sign up</button>
+    //     </form>
+    //     <br />
+    //     <button>
+    //       <Link to="/">Back</Link>
+    //     </button>
+    //   </div>
+     );
   }
 }
 
