0a3706cbae85815f20e867258313bea1fbefeed4
Kristen Maggard <ksm5k@virginia.edu>
2019-05-30 11:18:51 -0400

    working resume button, wikipedia link on help page

--- src/Help.js
+++ src/Help.js
@@ -10,7 +10,7 @@ class Help extends Component {
         <h1>Need Help?</h1> <br />
         <h1>FAQ</h1>
         <br />
-        <h2>What is the Pomodoro Technique? -> insert Link</h2> <br />
+        <h2>What is the Pomodoro Technique? <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique">Read about it!</a></h2> <br />
       </div>
     );
   }
